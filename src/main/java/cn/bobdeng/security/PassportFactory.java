7d9ccb7b07dc81f3a0896a5e792c2e92b01851ca
DengZhiGuo <bobdeng@163.com>
2021-04-24 16:17:50 +0800

    refactor

--- src/main/java/cn/bobdeng/security/PassportFactory.java
+++ src/main/java/cn/bobdeng/security/PassportFactory.java
@@ -1,7 +1,6 @@
 package cn.bobdeng.security;
 
 public interface PassportFactory {
-    Passport newPassport();
 
     Passport fromString(String value);
 }
--- src/test/java/cn/bobdeng/security/PassportFactoryImpl.java
+++ src/test/java/cn/bobdeng/security/PassportFactoryImpl.java
@@ -3,10 +3,6 @@ package cn.bobdeng.security;
 import com.google.gson.Gson;
 
 public class PassportFactoryImpl implements PassportFactory {
-    @Override
-    public Passport newPassport() {
-        return new TestPassport();
-    }
 
     @Override
     public Passport fromString(String value) {

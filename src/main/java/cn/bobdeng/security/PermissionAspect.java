29780398c78a19a2335d9c4326bedf74fd362698
DengZhiGuo <bobdeng@163.com>
2021-04-24 20:25:09 +0800

    refactor

--- src/main/java/cn/bobdeng/security/PermissionAspect.java
+++ src/main/java/cn/bobdeng/security/PermissionAspect.java
@@ -1,9 +1,11 @@
 package cn.bobdeng.security;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
 @Slf4j
+@Aspect
 public class PermissionAspect {
     private final PermissionRepository permissionRepository;
 

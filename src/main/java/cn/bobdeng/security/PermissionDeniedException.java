f31e93b7aeaa4eff8422fba0dd64720a9d615b6b
DengZhiGuo <bobdeng@163.com>
2021-04-24 20:31:09 +0800

    refactor

--- src/main/java/cn/bobdeng/security/PermissionDeniedException.java
+++ src/main/java/cn/bobdeng/security/PermissionDeniedException.java
@@ -1,4 +1,8 @@
 package cn.bobdeng.security;
 
 public class PermissionDeniedException extends RuntimeException {
+    @Override
+    public String getMessage() {
+        return "permission denied";
+    }
 }
